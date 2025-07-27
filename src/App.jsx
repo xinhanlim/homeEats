d29b36f9463a56c1e70f674f768a40cf18eb1852
xinhanlim <xhxhan00@gmail.com>
2025-07-27 06:10:12 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar'
 
 export default function App(){
   
   return(<>
 
+    <Navbar/>
     <h1 className="text-3xl font-bold underline">Hello World</h1>
   
   </>)
