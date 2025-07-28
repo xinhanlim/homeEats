8d6206f62bfb774780587f6dfcdfb1abf2110f10
xinhanlim <xhxhan00@gmail.com>
2025-07-28 14:56:42 +0000

    added padding to the h1

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,8 +4,8 @@ export default function HeroSection() {
             <div className="relative z-10 w-screen h-[80vh0 overflow-hidden ">
                 {/* <img className="absolute w-full h-full object-cover  " src={SectionImg} alt="photo of coffee" /> */}
                 <div className="z-20 relative flex inset-0 flex flex-col justify-left">
-                    <div className="container mx-auto px-0 py-40 ">
-                        <h1 className="playfair-display-h1 tracking-tighter px-4 text-5xl md:text-7xl lg:text-8xl font-bold text-[#282828] max-w-xl md:max-w-2xl lg:max-w-3xl">Your Daily Dose Of Premium Coffee</h1>
+                    <div className="container mx-auto px-4 py-40 ">
+                        <h1 className="playfair-display-h1 tracking-tighter text-5xl  md:text-7xl lg:text-8xl font-bold text-[#282828] max-w-xl md:max-w-2xl lg:max-w-3xl">Your Daily Dose Of Premium Coffee</h1>
                     </div>
 
                     {/* <div className="container mx-auto px-0 flex justify-end">
